0ad2c2a05a2d2bb123ea4f508e7196f4053d4ebf
fukunagaa <k.k.0627ku@gmail.com>
2020-07-27 11:20:51 +0900

    簡易的な画面表示まで

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Knight from "./components/Knight";
+
+const app = document.getElementById("app");
+ReactDOM.render(<Knight />, app);
